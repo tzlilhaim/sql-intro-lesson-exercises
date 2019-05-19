686f9ea8839f2f29a17e3e109313aaba42e7056b
jona <jona@elevation.ac>
2019-05-19 17:46:56 +0300

    change test db name in circleci config and utils file

--- tests/sql_test_utils.js
+++ tests/sql_test_utils.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const sqlConnectionConfig = {
     host: 'localhost',
     user: 'root',
-    database: 'sql_intro',
+    database: 'sql_testing',
     insecureAuth: true
 }
 
